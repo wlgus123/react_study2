96101632c05f2b929c87fe47c50ba0494d95448d
wlgus123 <mail30019794@gmail.com>
2024-01-23 16:01:12 +0900

    [:sparkles:] 스타일 작성

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   const style = {
     backgroundColor: 'black',
     color: 'aqua',
-    fontSize: 24,  // 기본 단위 px
+    fontSize: 24,  // 기본 단위 px 
     padding: '1rem'  // 다른 단위 사용 시 문자열로 설정
   }
 
