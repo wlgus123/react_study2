de23f8ebfc69987b39763f8e7aef2567bc018aac
wlgus123 <mail30019794@gmail.com>
2024-01-26 23:59:26 +0900

    11. 배열 렌더링하기 - map() 함수를 사용한 렌더링

--- src/UserList.js
+++ src/UserList.js
@@ -29,11 +29,9 @@ function UserList() {
 
     return (
         <div>
-            {/* 컴포넌트를 재사용 할 수 있도록 만들어줌.
-            하지만 배열의 인덱스를 하나하나 조회해가면서 렌더링하는 방법은 동적인 배열을 렌더링하지 못함 */}
-            <User user={users[0]} />
-            <User user={users[1]} />
-            <User user={users[2]} />
+            {users.map(user => (
+                <User user={user} />
+            ))}
         </div>
     )
 }
