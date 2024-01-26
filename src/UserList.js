6dc18216dc9566dde1db93bb313714556942103f
wlgus123 <mail30019794@gmail.com>
2024-01-26 23:52:30 +0900

    11. 배열 렌더링하기 - 컴포넌트를 재사용 할 수 있도록 수정

--- src/UserList.js
+++ src/UserList.js
@@ -1,5 +1,13 @@
 import React from "react";
 
+function User({ user }) {
+    return (
+        <div>
+            <b>{user.username}</b> <span>({user.email})</span>
+        </div>
+    );
+}
+
 function UserList() {
     const users = [
         {
@@ -21,15 +29,11 @@ function UserList() {
 
     return (
         <div>
-            <div>
-                <b>{users[0].username}</b> <span>({users[0].email})</span>
-            </div>
-            <div>
-                <b>{users[1].username}</b> <span>({users[1].email})</span>
-            </div>
-            <div>
-                <b>{users[2].username}</b> <span>({users[1].email})</span>
-            </div>
+            {/* 컴포넌트를 재사용 할 수 있도록 만들어줌.
+            하지만 배열의 인덱스를 하나하나 조회해가면서 렌더링하는 방법은 동적인 배열을 렌더링하지 못함 */}
+            <User user={users[0]} />
+            <User user={users[1]} />
+            <User user={users[2]} />
         </div>
     )
 }
