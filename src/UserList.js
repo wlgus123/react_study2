6aaf83e28935b3b6515296c453ee20c7ea601bd4
wlgus123 <mail30019794@gmail.com>
2024-01-27 23:47:22 +0900

    12. useRef로 컴포넌트 안의 변수 만들기

--- src/UserList.js
+++ src/UserList.js
@@ -8,29 +8,11 @@ function User({ user }) {
     );
 }
 
-function UserList() {
-    const users = [
-        {
-            id: 1,
-            username: 'velopert',
-            email: 'public.velopert@gmail.com'
-        },
-        {
-            id: 2,
-            username: 'tester',
-            email: 'tester@example.com'
-        },
-        {
-            id: 3,
-            username: 'liz',
-            email: 'liz@example.com'
-        }
-    ];
-
+function UserList({ users }) {
     return (
         <div>
-            {users.map(user => (
-                <User user={user} />
+            {users.map((user, index) => (
+                <User user={user} key={index} />
             ))}
         </div>
     )
