77eeeba423a58e340c0db773bff33b6f876518d2
wlgus123 <mail30019794@gmail.com>
2024-01-28 00:16:05 +0900

    13. 배열에 항목 추가하기 - concat 함수 사용

--- src/App.js
+++ src/App.js
@@ -40,7 +40,8 @@ function App() {
       username,
       email
     };
-    setUser([...users, user]);
+    // concat 함수는 기존의 배열을 수정하지 않고 새로운 원소가 추가된 새로운 배열을 만들어줌
+    setUser(users.concat(user));  
 
     setInputs({
       username: '',
